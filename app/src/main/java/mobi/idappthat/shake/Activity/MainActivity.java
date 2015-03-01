a39437c84987e14131c3608aa148fedfdf3e9340
MOBI <uta.mobi@gmail.com>
2015-03-01 03:55:10 -0600

    Facebook invite

--- app/src/main/java/mobi/idappthat/shake/Activity/MainActivity.java
+++ app/src/main/java/mobi/idappthat/shake/Activity/MainActivity.java
@@ -3,7 +3,11 @@ package mobi.idappthat.shake.Activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import mobi.idappthat.shake.Fragment.FacebookEventFragment;
 import mobi.idappthat.shake.Fragment.LoginFragment;
 import mobi.idappthat.shake.Fragment.MainFragment;
 import mobi.idappthat.shake.R;
@@ -24,6 +28,29 @@ public class MainActivity extends ActionBarActivity {
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new MainFragment())
                     .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                FacebookEventFragment facebookEventFragment = new FacebookEventFragment();
+                getFragmentManager().beginTransaction()
+                        .add(R.id.container, facebookEventFragment)
+                        .commit();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
--- app/src/main/java/mobi/idappthat/shake/Fragment/UserLikesFragment.java
+++ app/src/main/java/mobi/idappthat/shake/Fragment/UserLikesFragment.java
@@ -6,4 +6,5 @@ import android.support.v4.app.Fragment;
  * Created by Cameron on 2/28/15.
  */
 public class UserLikesFragment extends Fragment {
+
 }
